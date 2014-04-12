e649386a3b451075c79fda39143012df9a2a7892
Jeong Kwang Hyeon <sivistys@gmail.com>
2014-04-13 00:37:06 +0900

    hook class created

---
+++ Hook.cpp
@@ -0,0 +1,24 @@
+/*
+ *  Date: 2014-04-12
+ *	Version: 1.0
+ */
+
+#include "Hook.h"
+
+
+
+Hook::Hook(){
+	texture.loadFromFile("img/hook.png");
+	sprite.setTexture(texture);
+}
+
+Hook::~Hook(){
+}
+
+void Hook::update(){
+}
+
+void Hook::draw(RenderWindow &window){
+	sprite.draw(window);
+
+}
---
+++ Hook.h
@@ -0,0 +1,34 @@
+/*
+ *  Date: 2014-04-12
+ *	Version: 1.0
+ */
+
+#ifndef __HOOK_H__
+#define __HOOK_H__
+
+#include "stdafx.h"
+
+using namespace sf;
+using namespace std;
+
+class Hook{
+	private:
+		//Images
+		Texture texture;
+		Sprite sprite;
+
+		//Position Calculations
+		float currentDirection;
+		Vector2f currentPosition;
+
+	public:
+		Hook();
+		~Hook();
+		void update();
+		void draw(RenderWindow &window);
+
+}
+
+
+
+#endif
